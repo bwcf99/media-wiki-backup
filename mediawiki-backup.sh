f35b5f9a87e34f126a3789710406931cd5900d3e
Brent <brentwcf@mac.com>
2015-11-04 23:54:34 -0500

    html backups working

--- mediawiki-backup.sh
+++ mediawiki-backup.sh
@@ -11,6 +11,10 @@ date=$(date +"%y-%m-%d-%H-%M-%S")
 
 #Backup Paths#
 mysql_backup_path="/home/human/backups"
+wikiDirBackup="/home/human/backups"
+
+#Wiki Path
+wikiPath="/var/www/mediawiki"
 
 
 ### MySQL Backup ###
@@ -20,9 +24,22 @@ umask 177
 
 # Dump DB into SQL file #
 echo "Dumping DB into SQL file"
-mysqldump --user=$mysql_User --password=$mysql_Password --host=$mysql_host $mysql_DB > $mysql_backup_path/$mysql_DB-$date.sql
+mysqldump --user=$mysql_User --password=$mysql_Password --host=$mysql_host $mysql_DB > $mysql_backup_path/wiki-db.$date.sql
 
 #Remove backups older than 30 days #
 echo "Deleting all backups older than 30 days"
 
 find $mysql_backup_path/* -mtime +30 -exec rm {} \;
+find $wikiDirBackup/* -mtime +30 -exec rm {} \;
+
+echo "End MySQL Backup - Start Local File Backup"
+
+### Wiki Directory Backup ###
+echo ""
+echo "Starting the local backup - This may take a few moments!"
+tar cfz $wikiDirBackup/wiki-html.$date.tar.gz $wikiPath
+echo "Backed up and compressed the local wiki files"
+
+echo "############################"
+echo "# ~Wiki Backup Complete!!~ #"
+echo "############################"
